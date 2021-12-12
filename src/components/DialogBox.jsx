a9883ebf281d80b571934299f1cdc6c9100b46e3
chanchal16 <rajputchanchal17@gmail.com>
2021-12-12 22:45:09 +0530

    style- add hover effect

--- src/components/DialogBox.jsx
+++ src/components/DialogBox.jsx
@@ -19,6 +19,13 @@ const useStyles = makeStyles((theme)=>({
         // flexWrap:'wrap',
         margin: '15px'
     },
+    list:{
+        padding:'0 16px',
+        '&:hover':{
+            backgroundColor:'rgb(252, 211, 77,0.1)'
+        }
+
+    }
    
 }))
 
@@ -34,20 +41,22 @@ export default function DialogBox({onClose, selectedValue, open}) {
     return (
         <div>
              <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} className='dialogbox'>
-                <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+                <DialogTitle id="simple-dialog-title" style={{color:'#df9a29'}} >Categories</DialogTitle>
                
                 <div className={classes.accordiondiv}>
                     {
                         topicsData?.map((topic)=>{
                             return(
                                 <div key={topic.title} className={classes.details}>
-                                    <Typography style={{fontSize:'17px',color:'#a76a1d'}} >{topic.title} </Typography>
+                                    <Box style={{borderLeft:'4px solid #d97706',borderRadius:'5%',padding:'2px'}}>
+                                    <Typography style={{fontSize:'17px',color:'#d97706'}} >{topic.title} </Typography>
+                                    </Box>
                                     <List>
                                     {
                                         topic.topics?.map((t)=>{
                                             return(
                                                 <div className={classes.topicsbox} key={t.id}>
-                                                    <ListItem button onClick={()=>handleListItemClick(t.name)} style={{padding:'0 16px'}} >                                              
+                                                    <ListItem button onClick={()=>handleListItemClick(t.name)} className={classes.list} >                                              
                                                         <ListItemText primary={t.name} className='listItem' />
                                                     </ListItem>
                                                 </div>
