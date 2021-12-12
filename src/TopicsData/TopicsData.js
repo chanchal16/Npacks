916b84dbe6cf5b32844d7c340838138f20501b88
chanchal16 <rajputchanchal17@gmail.com>
2021-12-12 19:50:23 +0530

    Create Topics data

---
+++ src/TopicsData/TopicsData.js
@@ -0,0 +1,154 @@
+const topicsData = [
+    {
+      title: "JS Frameworks",
+      topics: [
+        {
+            id: 1,
+            name: "vue"
+        },
+        {
+            id: 2,
+            name: "react"
+        },
+        {
+            id: 3,
+            name: "angular"
+        },
+        {
+            id:4,
+            name:'emberjs'
+        },
+        {
+            id:5,
+            name:'backbonejs'
+        }
+      ]
+    },
+    {
+        title: "CSS Frameworks",
+        topics: [
+            {
+                id: 6,
+                name: "bootstrap"
+            },
+            {
+                id: 7,
+                name: "tailwind"
+            },
+            {
+                id: 8,
+                name: "Bulma"
+            }
+        ]
+    },
+    {
+        title:'Modals & Popups',
+        topics:[
+            {
+                id:9,
+                name:'Tooltip'
+            },
+            {
+                id:10,
+                name:'Notification'
+            },
+            {
+                id:11,
+                name:'Popup'
+            },
+            {
+                id:12,
+                name:'Dialog'
+            },
+            {
+                id:13,
+                name:'Toast'
+            }
+        ]
+    },
+    {
+        title:'Navigation',
+        topics:[
+            {
+                id:14,
+                name:'Accordion'
+            },
+            {
+                id:15,
+                name:'Infinte scroll'
+            },
+            {
+                id:16,
+                name:'tabs'
+            },
+            {
+                id:17,
+                name:'Pagination'
+            }
+        ]
+    },
+    {
+        title:'Testing frameworks',
+        topics:[
+            {
+                id:18,
+                name:'jest'
+            },
+            {
+                id:19,
+                name:'mocha'
+            },
+            {
+                id:20,
+                name:'jasmine'
+            }
+        ]
+    },
+    {
+        title:'File Formats',
+        topics:[
+            {
+                id:21,
+                name:'JSON to CSV'
+            },
+            {
+                id:22,
+                name:'DOCX to PDF'
+            },
+            {
+                id:23,
+                name:'DOCX Parser'
+            },
+            {
+                id:24,
+                name:'HTML Parser'
+            }
+        ]
+    },
+    {
+        title:'Other UI Components',
+        topics:[
+            {
+                id:25,
+                name:'animations'
+            },
+            {
+                id:26,
+                name:'carousel'
+            },
+            {
+                id:27,
+                name:'charts'
+            },
+            {
+                id:28,
+                name:'table'
+            },
+            {
+                id:29,
+                name:'Progress'
+            }
+        ]
+    }
+  ];
+  export default topicsData
