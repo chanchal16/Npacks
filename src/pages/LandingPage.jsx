63e2470961011576735bc6cbff04c9ac0be6b1da
chanchal16 <rajputchanchal17@gmail.com>
2021-12-25 23:16:38 +0530

    Update- added animations to elements

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import SearchBar from '../components/SearchBar';
 import { Box,Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Fade,Zoom} from "react-awesome-reveal";
+
 
 const font = "'Nunito', sans-serif ";
 const useStyles = makeStyles((theme)=>({
@@ -24,14 +26,14 @@ const useStyles = makeStyles((theme)=>({
         flexDirection:'row',
         justifyContent:'space-between',
         background:'#e3d7c1',
-        padding:'5rem 4rem'
+        padding:'3rem 4rem'
     },
     categorybox:{
         display:'flex',
         flexDirection:'row',
         justifyContent:'space-between',
-        background:'#5d4a32',
-        padding:'5rem 1rem'
+        // background:'#5d4a32',
+        padding:'3rem 1rem'
     },
     listheading:{
         margin:'auto',
@@ -43,7 +45,9 @@ const useStyles = makeStyles((theme)=>({
         height:'300px',
         padding:'1rem',
         margin:'1rem',
-        border:`2px solid ${theme.palette.primary.main}`,
+        borderLeft: '2px solid #df9a29',
+        borderTop: '2px solid #df9a29',
+        // border:`2px solid ${theme.palette.primary.main}`,
         // background:theme.palette.primary.main
     },
     upperscreenshot:{
@@ -52,10 +56,21 @@ const useStyles = makeStyles((theme)=>({
         // padding:'1rem',
         margin:'1rem',
         position: 'absolute',
-        // right: '-2rem',
+        right: '0rem',
+        top:0
         // border:`2px solid ${theme.palette.primary.main}`,
         // background:'black'
         // overflow:'hidden'
+    },
+    metricscontainer:{
+        background:'#e3d7c1',
+        padding:'2rem'
+    },
+    chartscontainer:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        margin:'1rem'
     }
 }))
 
@@ -65,47 +80,78 @@ export default function LandingPage() {
         <div>
             <Box className={classes.topcontainer}>
                 <Box className={classes.headingbox}>
-                    <Typography  variant='h4' className={classes.heading}> An extensive collection of open source packages to browse through.</Typography>
-                    <Typography variant='h6' className={classes.heading}>Choose the package that best matches your application</Typography>
+                    <Fade >
+                    <Typography  variant='h4' className={classes.heading}>
+                         An extensive collection of open source packages to browse through.
+                    </Typography>
+                    <Typography variant='h6' className={classes.heading}>
+                        Choose the package that best matches your application
+                    </Typography>
+                    </Fade>
                 </Box>
+
+                {/**search bar */}
                 <SearchBar />
+
                 <Link to='/home' style={{textDecoration:'none'}}>
-                    <Button variant="outlined" color="primary" style={{margin:'5rem auto 8rem'}}>Explore</Button>
+                    <Button variant="outlined" color="primary" style={{margin:'5rem auto 8rem'}}>
+                        Explore
+                    </Button>
                 </Link> 
             </Box>
-            <Box className={classes.packagelist} >
+
+            {/**packagaes section */}
+            <Box className={classes.packagelist} >         
                 <Box className={classes.listheading}>
-                <Typography variant='h4'>Select from a number of packages </Typography>
-                </Box>
-                <Box className={classes.screenshot}>
-                    <Box className={classes.upperscreenshot}>
-                        <img src='../.././assets/list.png' alt='packageslist' style={{width:'450px',height:'350px'}} />
-                    </Box>
-                </Box>       
+                    <Fade direction='left'>
+                        <Typography variant='h4'>Select from a number of packages </Typography>
+                    </Fade>
+                </Box>          
+                <Zoom>
+                    <Box className={classes.screenshot}>             
+                        <Box className={classes.upperscreenshot}>
+                            <img src='../.././assets/list.png' alt='packageslist' 
+                            style={{width:'450px',height:'350px'}} />
+                        </Box>           
+                    </Box> 
+                </Zoom>   
             </Box>
 
+            {/**category section */}
             <Box className={classes.categorybox} >
-                <Box className={classes.screenshot}>
-                    <Box className={classes.upperscreenshot}>
-                    <img src='../.././assets/catgories.png' alt='topics' style={{width:'450px',height:'350px'}} />
-                    </Box>
-                </Box>   
+                <Zoom>
+                    <Box className={classes.screenshot}>
+                        <Box className={classes.upperscreenshot}>
+                        <img src='../.././assets/catgories.png' alt='topics' style={{width:'450px',height:'350px'}} />
+                        </Box>
+                    </Box> 
+                </Zoom> 
                 <Box className={classes.listheading}>
-                    <Typography variant='h4'>Browse through a wide range of categories.</Typography>
-                </Box>
-                   
+                    <Fade direction='right'>
+                        <Typography variant='h4' color='primary'>Browse through a wide range of categories.</Typography>
+                    </Fade>
+                </Box>                
             </Box>
 
-            <Box className={classes.packagelist} >
-                <Box className={classes.listheading}>
-                <Typography variant='h4'> Learn about the packages through their statistics and 
-                metrics. </Typography>
-                </Box>
-                <Box className={classes.screenshot}>
-                    <Box className={classes.upperscreenshot}>
-                    <img src='../.././assets/charts.png' alt='charts' style={{width:'450px',height:'350px'}} />
-                    </Box>
-                </Box>       
+            {/**charts section */}
+            <Box className={classes.metricscontainer} >
+                <Fade>
+                    <Typography variant='h5'>Learn about the packages through their statistics and 
+                    metrics.</Typography>
+                </Fade>
+
+                <Fade delay={300} cascade>
+                    <Box className={classes.chartscontainer}>
+                            <Box className={classes.charts}>
+                                <img src='../.././assets/charts.png' alt='charts' 
+                                style={{width:'500px',height:'350px'}} />
+                            </Box>
+                            <Box className={classes.charts}>
+                                <img src='../.././assets/charts.png' alt='charts' 
+                                style={{width:'500px',height:'350px'}} />
+                            </Box>
+                    </Box> 
+                </Fade>       
             </Box>
         </div>
     )
