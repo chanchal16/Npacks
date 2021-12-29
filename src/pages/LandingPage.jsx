d3aece32156a37ff02b68f45258f5e33b5c2f625
chanchal16 <rajputchanchal17@gmail.com>
2021-12-29 14:37:43 +0530

    Update-add breakpoints for mobile responsiveness

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,11 +8,6 @@ import { Fade,Zoom} from "react-awesome-reveal";
 
 const font = "'Nunito', sans-serif ";
 const useStyles = makeStyles((theme)=>({
-    topcontainer:{
-        position:'relative'
-        // height:'100vh',
-        // background:theme.palette.primary.dark
-    },
     headingbox:{
         color:theme.palette.primary.light,
         padding:'1rem',
@@ -20,25 +15,63 @@ const useStyles = makeStyles((theme)=>({
         
     },
     heading:{
-        fontFamily:font
+        fontFamily:font,
+        [theme.breakpoints.down('md')]:{
+            fontSize:'18px'
+        }
+    },
+    subheading:{
+        fontFamily:font,
+        [theme.breakpoints.down('md')]:{
+            fontSize:'15px'
+        }
+    },
+    images:{
+        width:'450px',
+        height:'350px',
+        [theme.breakpoints.down('md')]:{
+            width:'300px',
+            height:'200px'
+        },
+        [theme.breakpoints.down(768)]:{
+            width:'250px',
+            height:'150px'
+        },
+        ['@media (width:280px)']: {
+            width:'230px',
+            height:'130px'
+        }
     },
     packagelist:{
         display:'flex',
         flexDirection:'row',
         justifyContent:'space-between',
         background:'#e3d7c1',
-        padding:'3rem 4rem'
+        padding:'3rem 4rem',
+        [theme.breakpoints.down(768)]:{
+            flexDirection:'column',
+            padding:'1rem'
+        }
     },
     categorybox:{
         display:'flex',
         flexDirection:'row',
         justifyContent:'space-between',
         // background:'#5d4a32',
-        padding:'3rem 1rem'
+        padding:'3rem 1rem',
+        [theme.breakpoints.down(768)]:{
+            flexDirection:'column',
+            padding:'1rem'
+        }
     },
     listheading:{
         margin:'auto',
-        width: '50%'
+        // width: '50%',
+        fontSize:'2.1rem',
+        [theme.breakpoints.down(768)]:{
+            fontSize:'1.2rem',
+            
+        }
     },
     screenshot:{
         position:'relative',
@@ -46,10 +79,25 @@ const useStyles = makeStyles((theme)=>({
         height:'300px',
         padding:'1rem',
         margin:'1rem',
-        borderLeft: '2px solid #df9a29',
-        borderTop: '2px solid #df9a29',
-        // border:`2px solid ${theme.palette.primary.main}`,
-        // background:theme.palette.primary.main
+        borderLeft: `2px solid ${theme.palette.primary.main}`,
+        borderTop: `2px solid ${theme.palette.primary.main}`,
+        [theme.breakpoints.down('md')]:{
+            width:'300px',
+            height:'200px',
+            margin:'auto'        
+        },
+        [theme.breakpoints.down(768)]:{
+            width:'250px',
+            height:'150px',
+            border:`2px solid ${theme.palette.primary.main}`
+        },
+        ['@media (width:280px)']: {
+            width:'230px',
+            height:'130px',
+            padding:'0.5rem',
+            margin:'0.5rem',
+            border:'none'
+        }
     },
     upperscreenshot:{
         width:'400px',
@@ -58,20 +106,43 @@ const useStyles = makeStyles((theme)=>({
         margin:'1rem',
         position: 'absolute',
         right: '0rem',
-        top:0
-        // border:`2px solid ${theme.palette.primary.main}`,
-        // background:'black'
-        // overflow:'hidden'
+        top:0,
+        [theme.breakpoints.down('md')]:{
+            width:'300px',
+            height:'200px'
+        },
+        [theme.breakpoints.down(768)]:{
+            width:'250px',
+            height:'150px'
+        },
+        ['@media (width:280px)']: {
+            width:'230px',
+            height:'130px',
+        }
     },
     metricscontainer:{
         background:'#e3d7c1',
-        padding:'2rem'
+        padding:'2rem',
+        fontSize:'1.8rem',
+        [theme.breakpoints.down(768)]:{
+            fontSize:'1.5rem'
+        },
+        ['@media (width:280px)']: {
+            padding:'1rem',
+            fontSize:'1.2rem'
+        }
     },
     chartscontainer:{
         display:'flex',
         flexDirection:'row',
         justifyContent:'space-evenly',
-        margin:'1rem'
+        margin:'1rem',
+        [theme.breakpoints.down(768)]:{
+            flexDirection:'column'
+        },
+        ['@media (width:280px)']: {
+            margin:'auto'
+        }
     }
 }))
 
@@ -85,7 +156,7 @@ export default function LandingPage() {
                     <Typography  variant='h4' className={classes.heading}>
                          An extensive collection of open source packages to browse through.
                     </Typography>
-                    <Typography variant='h6' className={classes.heading}>
+                    <Typography variant='h6' className={classes.subheading}>
                         Choose the package that best matches your application
                     </Typography>
                     </Fade>
@@ -107,14 +178,14 @@ export default function LandingPage() {
             <Box className={classes.packagelist} >         
                 <Box className={classes.listheading}>
                     <Fade direction='left'>
-                        <Typography variant='h4'>Select from a number of packages </Typography>
+                        <p>Select from a number of packages </p>
                     </Fade>
                 </Box>          
                 <Zoom>
                     <Box className={classes.screenshot}>             
                         <Box className={classes.upperscreenshot}>
                             <img src='../.././assets/list.png' alt='packageslist' 
-                            style={{width:'450px',height:'350px'}} />
+                            className={classes.images} />
                         </Box>           
                     </Box> 
                 </Zoom>   
@@ -125,13 +196,13 @@ export default function LandingPage() {
                 <Zoom>
                     <Box className={classes.screenshot}>
                         <Box className={classes.upperscreenshot}>
-                        <img src='../.././assets/catgories.png' alt='topics' style={{width:'450px',height:'350px'}} />
+                        <img src='../.././assets/catgories.png' alt='topics' className={classes.images} />
                         </Box>
                     </Box> 
                 </Zoom> 
                 <Box className={classes.listheading}>
                     <Fade direction='right'>
-                        <Typography variant='h4' color='primary'>Browse through a wide range of categories.</Typography>
+                        <p style={{color:'#df9a29'}}>Browse through a wide range of categories.</p>
                     </Fade>
                 </Box>                
             </Box>
@@ -139,19 +210,19 @@ export default function LandingPage() {
             {/**charts section */}
             <Box className={classes.metricscontainer} >
                 <Fade>
-                    <Typography variant='h5'>Learn about the packages through their statistics and 
-                    metrics.</Typography>
+                    <p >Learn about the packages through their statistics and 
+                    metrics.</p>
                 </Fade>
 
                 <Fade delay={300} cascade>
                     <Box className={classes.chartscontainer}>
                             <Box className={classes.charts}>
                                 <img src='../.././assets/charts.png' alt='charts' 
-                                style={{width:'500px',height:'350px'}} />
+                                className={classes.images} />
                             </Box>
                             <Box className={classes.charts}>
                                 <img src='../.././assets/vue.png' alt='charts' 
-                                style={{width:'500px',height:'350px'}} />
+                                className={classes.images} />
                             </Box>
                     </Box> 
                 </Fade>       
