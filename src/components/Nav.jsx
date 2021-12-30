29b2602ef99e6e1ee9edcaa3e359eb57f15cf32c
chanchal16 <rajputchanchal17@gmail.com>
2021-12-30 21:57:12 +0530

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme)=>({
     appbar: {
         color:theme.palette.secondary.dark,
-        background:theme.palette.primary.dark,
+        background:theme.palette.primary.main,
         
     },
   }));
