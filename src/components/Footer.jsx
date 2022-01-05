d589d86d210a9c5866e99c0a646e1a48fa3dd4a2
chanchal16 <rajputchanchal17@gmail.com>
2022-01-05 14:21:48 +0530

    update-added path to social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PersonIcon from '@material-ui/icons/Person';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import { Box,Link, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -27,13 +28,16 @@ export default function Footer() {
     return (
         <div className={classes.footer}>
             <Box className={classes.socials}>
-                <Link href='' className={classes.links}>
+                <Link href='https://github.com/chanchal16' target="_blank" className={classes.links}>
                     <GitHubIcon />
                 </Link>
-                <Link href='' className={classes.links}>
+                <Link href='https://www.linkedin.com/in/rajput07/' target="_blank" className={classes.links}>
                     <LinkedInIcon />
                 </Link>
-                <Link href='' className={classes.links}>
+                <Link href='https://twitter.com/chanchal16_' target="_blank" className={classes.links}>
+                    <TwitterIcon />
+                </Link>
+                <Link href='https://chanchal.netlify.app/' target="_blank" className={classes.links}>
                     <PersonIcon />
                 </Link>
             </Box>
