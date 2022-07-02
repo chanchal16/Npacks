d9116e6d95a4337a7e6d64bf5c1578776b573673
chanchal16 <rajputchanchal17@gmail.com>
2022-07-02 22:53:32 +0530

    Create images.js

---
+++ public/images.js
@@ -0,0 +1,13 @@
+import categories from '../assets/catgories.png';
+import favicon  from '../assets/favicon.svg'
+import  fireflake  from '../assets/fireflake.svg';
+import  list from '../assets/list.png';
+import nodata  from '../assets/nodata.svg';
+import  pagenotfound  from '../assets/pagenotfound.svg';
+import react  from '../assets/react.png';
+import tailwind  from '../assets/tailwind.png';
+import empty  from '../assets/void.svg'
+
+export default{
+    categories,favicon, fireflake,list,nodata,pagenotfound,react,tailwind,empty
+}
